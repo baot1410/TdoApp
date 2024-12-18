5fa453171125f61b6624e5e370d84796984a4511
baot1410 <baot1410@uw.edu>
2024-12-17 17:22:40 -0800

    final #1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import Header from "./components/Header";
+import AddTask from "./components/AddTask";
+import ShowTask from "./components/ShowTask";
+
+function App() {
+  const [taskList, setTasklist] = useState(JSON.parse(localStorage.getItem("taskList")) || []);
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+  }, [taskList]);
+
+  return (
+    <div className="App">
+      <Header />
+      <AddTask 
+        taskList={taskList} 
+        setTasklist={setTasklist} 
+        task = {task}
+        setTask = {setTask}
+      />
+      <ShowTask
+        taskList={taskList} 
+        setTasklist={setTasklist}
+        task = {task}
+        setTask = {setTask}
+      />
+    </div>
+  );
+}
+
+export default App;
